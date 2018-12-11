bc7dfcf0fc0fff2d964b34afeb4ffe2961ab6835
Charlie <heinercj@gmail.com>
2018-12-11 10:57:34 -0800

    started room object setup

---
+++ src/components/rooms.js
@@ -0,0 +1,16 @@
+const ovalOffice = {
+  title: 'Oval Office',
+  image: 'ovalOffice.jpg',
+  description: '',
+  items: [],
+  people: [],
+  doors: {
+    w: 'study',
+    n: 'corridor',
+    e: 'westColonnade'
+  }
+};
+
+export const rooms = {
+  ovalOffice
+};
