553eaa8bd693a0f0b6a5bfc75475515a60998e82
Charlie <heinercj@gmail.com>
2018-12-11 10:23:35 -0800

    getting hello world on page. added babel loader to webpack config. added babel preset react to json. init app files

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,15 @@ module.exports = {
           }
         ]
       },
+      { 
+        test: /\.jsx?$/,         // Match both .js and .jsx files
+        exclude: /node_modules/, 
+        loader: 'babel-loader', 
+        query:
+          {
+            presets:['react']
+          }
+      },
       {
         test: /\.(jpg|png|svg)$/,
         use: {
